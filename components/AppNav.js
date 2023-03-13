d441917809bbbd0efe1b189f530e1406391719a9
Jairon Landa <jaironlanda@yahoo.com>
2023-03-13 20:51:57 +0800

    update nav

--- components/AppNav.js
+++ components/AppNav.js
@@ -1,16 +1,26 @@
+import Link from 'next/link';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 export default function AppNav() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">NewbAPI</Navbar.Brand>
+      <Link href={'/'} passHref legacyBehavior>
+        <Navbar.Brand>NewbAPI</Navbar.Brand>
+      </Link>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Explore</Nav.Link>
-            <Nav.Link href="#link">Fake API</Nav.Link>
-            <Nav.Link href="#link">API Status</Nav.Link>
+            <Link href={'/explore'} passHref legacyBehavior>
+              <Nav.Link>Explore</Nav.Link>
+            </Link>
+            <Link href={'/fake-api'} passHref legacyBehavior>
+              <Nav.Link>Fake API</Nav.Link>
+            </Link>
+            <Link href={'/'} passHref legacyBehavior>
+              <Nav.Link>API Status</Nav.Link>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
