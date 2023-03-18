f2c9889dd15555bc519610c68a6ab0dec4e0d6ea
Jairon Landa <jaironlanda@yahoo.com>
2023-03-18 11:35:27 +0800

    add google analytic

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+        </Script>
       <Script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js" />
       <link href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"></link>
